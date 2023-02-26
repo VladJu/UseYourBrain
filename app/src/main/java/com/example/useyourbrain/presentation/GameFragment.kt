915f3f281aeb3b80bd9b809e65cd7772f8a46e4a
VladJu <juikovvlad@gmail.com>
2023-02-26 16:06:34 +0300

    implementation View Binding instead of findViewById

--- app/src/main/java/com/example/useyourbrain/presentation/GameFragment.kt
+++ app/src/main/java/com/example/useyourbrain/presentation/GameFragment.kt
@@ -6,13 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.useyourbrain.R
+import com.example.useyourbrain.databinding.FragmentGameBinding
+import com.example.useyourbrain.databinding.FragmentWelcomeBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding : FragmentGameBinding?=null
+    private val binding = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding=FragmentGameBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
